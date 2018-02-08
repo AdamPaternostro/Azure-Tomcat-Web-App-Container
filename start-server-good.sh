ee958bd9088c430dd59bd2a78186ce696599234c
Adam Paternostro <AdamPaternostro@users.noreply.github.com>
2018-02-08 16:03:33 -0500

    Add files via upload

--- start-server-good.sh
+++ start-server-good.sh
@@ -3,21 +3,8 @@
 # Start Tomcat
 /usr/local/apache-tomcat-9.0.4/bin/startup.sh start
 
-# Test with curl that our site is up and is running (all warmed up)
-echo "Site test"
-while true;
-do
-    result=`curl http://localhost:8080/sample -k -s -f -o /dev/null && echo "UP" || echo "DOWN"`
-    if [ "$result" = "UP" ]
-    then
-        break
-    else
-        echo "Site not up"
-        sleep 1
-    fi
-done
-echo "Site ready"
-
+# Pretent Tomcat is warming up
+sleep 3m
 
 # Start Apache (foreground so the process does not end)
 /usr/sbin/apache2 -k start -DFOREGROUND
